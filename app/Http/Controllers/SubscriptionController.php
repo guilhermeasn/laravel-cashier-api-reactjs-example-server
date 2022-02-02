65b3b727a6d6267ed575c0acec705f2cc2c43015
guilhermeasn <guilhermeasn@yahoo.com.br>
2022-02-02 15:57:46 -0300

    improved subscription controller

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Laravel\Cashier\Cashier;
 
 class SubscriptionController extends Controller {
